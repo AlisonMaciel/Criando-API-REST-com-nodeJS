68de2d08678285736feaae9839cb4da2abfe2a0a
Alison <alisonbetini27@gmail.com>
2025-10-20 14:20:54 -0300

    configuring input port for render

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import { app } from './app.ts'
 import { env } from './env/index.ts'
 
+const PORT = env.PORT || process.env.PORT || 3333
+
+const numberPort = Number(PORT)
+
 app
   .listen({
-    port: env.PORT,
+    port: numberPort,
   })
   .then(() => {
     console.log('Server is running')
